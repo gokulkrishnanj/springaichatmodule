45be7e5de5e64430cb1c67d1e6074e5006c0fa79
gokulkrishnanj <gokulkrishnanj27@gmail.com>
2025-07-08 23:11:42 +0530

    Handled API key missing exception

--- src/main/java/com/example/springaichatmodel/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/springaichatmodel/Exceptions/GlobalExceptionHandler.java
@@ -1,21 +1,34 @@
 package com.example.springaichatmodel.Exceptions;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.retry.NonTransientAiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, String>> runTimeException(Exception e) {
+        log.info("RuntimeException");
         Map<String, String> errorResponseMap = new HashMap<>();
-        errorResponseMap.put("error", e.getLocalizedMessage());
+        errorResponseMap.put("errorMessage", e.getLocalizedMessage());
         return new ResponseEntity<>(errorResponseMap, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NonTransientAiException.class)
+    public ResponseEntity<Map<String,String>> nonTransientAiException(Exception exception){
+        log.info("NonTransientAiException");
+        Map<String, String> responseMap = new HashMap<>();
+        responseMap.put("errorMessage", exception.getLocalizedMessage());
+        return new ResponseEntity<>(responseMap, HttpStatus.BAD_REQUEST);
     }
 }
