c99f9a20a1a772501dcd0edcae3df7ddb86e12fa
gokulkrishnanj <gokulkrishnanj27@gmail.com>
2025-07-16 02:33:05 +0530

    Added advisor package

---
+++ src/main/java/com/example/springaichatmodel/Configuration/Advisors/ManageChatMemoryAdvisor.java
@@ -0,0 +1,18 @@
+package com.example.springaichatmodel.Configuration.Advisors;
+
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ManageChatMemoryAdvisor {
+
+    @Bean
+    public ChatMemory chatMemory(){
+        return MessageWindowChatMemory
+                .builder()
+                .maxMessages(20)
+                .build();
+    }
+}
--- src/main/java/com/example/springaichatmodel/Configuration/ChatClientConfiguration.java
+++ src/main/java/com/example/springaichatmodel/Configuration/ChatClientConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,18 +16,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatClientConfiguration {
 
+    @Autowired
+    private ChatMemory chatMemory;
 
     @Bean
     public ChatClient customChatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
         return chatClientBuilder.defaultAdvisors(new SafeGuardAdvisor(Constants.restrictedWordsList), new SimpleLoggerAdvisor(), MessageChatMemoryAdvisor.builder(chatMemory).build()).build();
     }
 
-    @Bean
-    public ChatMemory chatMemory() {
-        return MessageWindowChatMemory
-                .builder()
-                .maxMessages(20)
-                .build();
-    }
 
 }
