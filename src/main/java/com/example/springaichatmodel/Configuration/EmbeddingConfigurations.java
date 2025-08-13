fcca1705fc05c1e2b4da1b6c694bf66712ee913d
gokulkrishnanj <gokulkrishnanj27@gmail.com>
2025-08-13 15:31:46 +0530

    Renamed the embedding mode configuration class name.

--- src/main/java/com/example/springaichatmodel/Configuration/EmbeddingConfigurations.java
+++ src/main/java/com/example/springaichatmodel/Configuration/EmbeddingConfigurations.java
@@ -1,7 +1,5 @@
 package com.example.springaichatmodel.Configuration;
 
-import io.qdrant.client.QdrantClient;
-import io.qdrant.client.QdrantGrpcClient;
 import org.springframework.ai.ollama.OllamaEmbeddingModel;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaOptions;
@@ -11,29 +9,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
 @Configuration
-public class QdrantClinetConfiguration {
-
-    @Value("${spring.ai.vectorstore.qdrant.host}")
-    private String qdrantHost;
-
-    @Value("${spring.ai.vectorstore.qdrant.port}")
-    private int qdrantPort;
-
-    @Value("${spring.ai.vectorstore.qdrant.api-key}")
-    private String qdrantAPIKey;
+public class EmbeddingConfigurations {
 
     @Value("${spring.ai.ollama.embedding.model}")
     private String ollamaEmbeddingModel;
-
-
-    @Bean
-    @Lazy
-    public QdrantClient qdrantClient() {
-        QdrantGrpcClient.Builder qdrantGRPCClientBuilder = QdrantGrpcClient
-                .newBuilder(qdrantHost, qdrantPort, false);
-        qdrantGRPCClientBuilder.withApiKey(qdrantAPIKey);
-        return new QdrantClient(qdrantGRPCClientBuilder.build());
-    }
 
     @Bean
     @Lazy
@@ -43,9 +22,10 @@ public class QdrantClinetConfiguration {
                 .baseUrl("http://localhost:11434")
                 .build();
     }
+
     @Bean
     @Lazy
-    public OllamaOptions ollamaOptions(){
+    public OllamaOptions ollamaOptions() {
         return OllamaOptions
                 .builder()
                 .model(ollamaEmbeddingModel)
@@ -53,7 +33,7 @@ public class QdrantClinetConfiguration {
     }
 
     @Bean
-    public OllamaEmbeddingModel ollamaEmbeddingModel(OllamaApi ollamaApi, OllamaOptions ollamaOptions){
+    public OllamaEmbeddingModel ollamaEmbeddingModel(OllamaApi ollamaApi, OllamaOptions ollamaOptions) {
         return OllamaEmbeddingModel
                 .builder()
                 .defaultOptions(ollamaOptions)
