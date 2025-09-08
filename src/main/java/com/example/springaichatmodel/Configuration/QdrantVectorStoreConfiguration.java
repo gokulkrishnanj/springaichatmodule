a5e7724603eada4b7bee9932cf206db68caf9898
gokulkrishnanj <gokulkrishnanj27@gmail.com>
2025-09-08 22:40:22 +0530

    Added logs.

--- src/main/java/com/example/springaichatmodel/Configuration/QdrantVectorStoreConfiguration.java
+++ src/main/java/com/example/springaichatmodel/Configuration/QdrantVectorStoreConfiguration.java
@@ -2,11 +2,13 @@ package com.example.springaichatmodel.Configuration;
 
 import io.qdrant.client.QdrantClient;
 import io.qdrant.client.QdrantGrpcClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class QdrantVectorStoreConfiguration {
 
@@ -25,6 +27,9 @@ public class QdrantVectorStoreConfiguration {
     @Bean
     @Lazy
     public QdrantClient qdrantClient() {
+        log.info("host:"+ qdrantHost);
+        log.info("port:"+ qdrantPort);
+        log.info("useTLS:"+ useTransportLayerSecurity);
         QdrantGrpcClient.Builder qdrantGRPCClientBuilder = QdrantGrpcClient
                 .newBuilder(qdrantHost, 6334, useTransportLayerSecurity);
         qdrantGRPCClientBuilder.withApiKey(qdrantAPIKey);
