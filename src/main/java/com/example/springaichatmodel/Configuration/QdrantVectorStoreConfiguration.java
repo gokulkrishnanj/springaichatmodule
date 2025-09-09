03834598348b9869b75ead273f1fe90b67905603
gokulkrishnanj <gokulkrishnanj27@gmail.com>
2025-09-09 23:31:40 +0530

    Added env file config to fetch properties from .env

--- src/main/java/com/example/springaichatmodel/Configuration/QdrantVectorStoreConfiguration.java
+++ src/main/java/com/example/springaichatmodel/Configuration/QdrantVectorStoreConfiguration.java
@@ -19,11 +19,14 @@ public class QdrantVectorStoreConfiguration {
     @Value("${spring.ai.vectorstore.qdrant.api-key}")
     private String qdrantAPIKey;
 
+    @Value("${spring.ai.vectorstore.qdrant.use-tls}")
+    private boolean useTransportLayerSecurity;
+
     @Bean
     @Lazy
     public QdrantClient qdrantClient() {
         QdrantGrpcClient.Builder qdrantGRPCClientBuilder = QdrantGrpcClient
-                .newBuilder(qdrantHost, qdrantPort, false);
+                .newBuilder(qdrantHost, qdrantPort, useTransportLayerSecurity);
         qdrantGRPCClientBuilder.withApiKey(qdrantAPIKey);
         return new QdrantClient(qdrantGRPCClientBuilder.build());
     }
