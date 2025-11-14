49261f5405eddb19635b1cd6336d741a08216947
gokul.krishnan@workfall.com <gokulkrishnanj27@gmail.com>
2025-11-14 16:55:54 +0530

    Ollama configurations

---
+++ src/main/java/com/example/springaichatmodel/Configuration/OllamaConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.springaichatmodel.Configuration;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class OllamaConfiguration {
+    @Bean
+    @Primary
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+        return ChatClient.builder(ollamaChatModel).build();
+    }
+}
