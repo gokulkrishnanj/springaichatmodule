2f63dadee432709c49743db0283b3486f95ec879
Gokul J <gokulkrishnanj27@gmail.com>
2025-11-22 20:00:47 +0530

    Added log.

--- src/main/java/com/example/springaichatmodel/Controller/HealthController.java
+++ src/main/java/com/example/springaichatmodel/Controller/HealthController.java
@@ -1,14 +1,17 @@
 package com.example.springaichatmodel.Controller;
 
 import com.example.springaichatmodel.API.Health;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Slf4j
 @RestController
 public class HealthController implements Health {
     public ResponseEntity<String> getHealth() {
+        log.info("Health Controller");
         return new ResponseEntity<>("Health", HttpStatus.OK);
     }
 }
