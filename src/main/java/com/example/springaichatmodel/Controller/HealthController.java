8b3600cca62c1eaebd6b4298baf15d472483e9b6
Gokul J <gokulkrishnanj27@gmail.com>
2025-11-25 09:52:37 +0530

    Modified actions config

--- src/main/java/com/example/springaichatmodel/Controller/HealthController.java
+++ src/main/java/com/example/springaichatmodel/Controller/HealthController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController implements Health {
     public ResponseEntity<String> getHealth() {
-        log.info("Health Controller");
         return new ResponseEntity<>("Health", HttpStatus.OK);
     }
 }
