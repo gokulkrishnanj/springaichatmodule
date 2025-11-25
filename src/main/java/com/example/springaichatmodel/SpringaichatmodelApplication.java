3ed5b4ae39e07e9985f38068eaa45b18b36b3385
Gokul J <gokulkrishnanj27@gmail.com>
2025-11-25 14:15:20 +0530

    Added dependency, annotation and properties for eureka client

--- src/main/java/com/example/springaichatmodel/SpringaichatmodelApplication.java
+++ src/main/java/com/example/springaichatmodel/SpringaichatmodelApplication.java
@@ -2,8 +2,10 @@ package com.example.springaichatmodel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringaichatmodelApplication {
 
     public static void main(String[] args) {
